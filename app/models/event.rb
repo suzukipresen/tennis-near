48926591cc6a98f18224a02e670483e1bc70c7f7
鈴木紫織 <suzuki.presen@gmail.com>
2026-06-11 07:45:48 +0900

    ゲストログインとバリデーション機能を実装

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ApplicationRecord
-  belongs_to :user, optional: true #認証機能できたらop削除
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :event_date, presence: true
+  validates :place, presence: true
+  validates :description, presence: true
+  validates :capacity, presence: true
+  validates :fee, presence: true
 end
