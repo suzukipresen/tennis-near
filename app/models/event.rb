b6535453b58f267824ee22159ce4888e3597fce1
鈴木紫織 <suzuki.presen@gmail.com>
2026-06-14 14:44:57 +0900

    イベント投稿のバリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,17 @@ class Event < ApplicationRecord
   validates :event_date, presence: true
   validates :place, presence: true
   validates :description, presence: true
-  validates :capacity, presence: true
-  validates :fee, presence: true
+
+  validates :capacity,presence: true,numericality: { greater_than_or_equal_to: 1 }
+  validates :fee,presence: true,numericality: { greater_than_or_equal_to: 0 }
+  validates :level,presence: true,numericality: { greater_than_or_equal_to: 1,less_than_or_equal_to: 3}
+  validate :event_date_cannot_be_in_the_past
+
+  private
+
+  def event_date_cannot_be_in_the_past
+    return if event_date.blank?
+
+    errors.add(:event_date, "は現在以降を指定してください") if event_date < Time.current
+  end
 end
